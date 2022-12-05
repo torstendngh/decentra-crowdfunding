0c80f6b02fcab5ac53c1e8b51223c767778712c7
O'Donoghue, Torsten <torsten.odonoghue@student.fhws.de>
2022-12-05 21:34:23 +0100

    Tabs working

--- src/components/NavbarTabButton.jsx
+++ src/components/NavbarTabButton.jsx
@@ -1,9 +1,17 @@
 import "./NavbarTabButton.css"
+import { useNavigate } from 'react-router-dom'
 
-function NavbarTabButton({ icon, title, activeTab }) {
+function NavbarTabButton({ icon, title, activeTab, to }) {
+
+  const navigate = useNavigate()
 
   return (
-    <button className={`NavbarTabButton ${activeTab ? "ActiveTab" : ""}`}>
+    <button
+      to={to}
+      className={"NavbarTabButton " + `${window.location.pathname == to ? "ActiveTab" : ""}`}
+      type="button"
+      onClick={() => navigate(to)}
+    >
       {icon}
       {title}
     </button>
