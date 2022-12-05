56839f12bf5d9674c26b04f6ae7dd350d27a3feb
O'Donoghue, Torsten <torsten.odonoghue@student.fhws.de>
2022-12-05 16:58:47 +0100

    UI refresh and improved discover page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,8 +8,10 @@ function Card({ size, imgSrc, category, title, percentage, currentAmount, comple
       <div className="Details">
         <p className="Category">{category}</p>
         <p className="Title">{title}</p>
+        <div className="Flex"></div>
         <p className="Percentage">{percentage}% <p className="Amount">{currentAmount} of {completedAmount} {currency}</p></p>
       </div>
+      <div className="ProgressBar" style={{width: `${percentage}%`}}></div>
     </button>
   );
 }
