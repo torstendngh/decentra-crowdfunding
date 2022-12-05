0265ccbffcdd790bf787a8dc4ea9a7487981a30c
O'Donoghue, Torsten <torsten.odonoghue@student.fhws.de>
2022-12-05 21:30:41 +0100

    Implemented routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
 import DiscoverPage from './pages/DiscoverPage';
 import './vars.css'
 import './normalize.css'
 import './pages/CommonPageStyles.css'
+import ProjectPage from './pages/ProjectPage';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <div className="Page Flex">
-        <DiscoverPage/>
-      </div>
+      <BrowserRouter>
+        <Navbar/>
+        <div className="Page Flex">
+          <Routes>
+            <Route index element={<DiscoverPage/>}/>
+            <Route path="project" element={<ProjectPage/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
